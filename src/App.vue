1d43224f7f34b7db0b54ad909d50d32c18938377
phillipeismark <phillip_eismark@hotmail.dk>
2024-01-22 17:48:26 +0100

    Create component with forms using 2-way binding and log out the inputs

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <template>
   <Navbar :pages="pages" :active-page="activePage" :nav-link-click="(index) => activePage = index" />
-  <PageViewer v-if="pages.length > 0" :page="pages[activePage]" />
+  <!-- <PageViewer v-if="pages.length > 0" :page="pages[activePage]" /> -->
+
+  <!--
+    Creating a form to create pages.
+    page-created will be a callback.
+    It will be passed a method pageCreated defined in our method options
+    The callback will take a page in, and add it to the pages array.
+  -->
+  <CreatePage :page-created="pageCreated" />
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue';
 import PageViewer from './components/PageViewer.vue';
+import CreatePage from './components/CreatePage.vue';
 
 export default {
   components: {
     Navbar,
     PageViewer,
+    CreatePage
   },
   created() {
     this.getPages();
@@ -31,6 +41,9 @@ export default {
       } catch (error) {
         console.log('error', error)
       }
+    },
+    pageCreated(pageObj) {
+      console.log('pageObj', pageObj);
     }
   }
 }
---
+++ src/components/CreatePage.vue
@@ -0,0 +1,46 @@
+<!-- 
+  This component, defines a prop 'pageCreated' that it can get passed.
+  This prop, is used in the @click event handler where it's passed the pageTitle data variable.
+  So, whatever is passed from a parent, to this component under the pageCreated prop, will get called with the argument pageTitle.
+  In order to update the pageTitle defined in the data, we bind the value of the input, to the data variable pageTitle.
+  If we didn't do that, the pageTitle would keep the value it was initialized with, and not update.
+-->
+
+<template>
+  <div class="container mb-3">
+    <form action="">
+      <div class="mb-3">
+        <label for="" class="form-label">
+          Page Title
+        </label>
+        <!-- 
+          Bind our data variable 'pageTitle' to the input value with 2-way binding (v-model).
+          We do this, to be able to pass the value from the input, to our pageCreated method passed from the parent App.vue.
+          Note that the alternative to v-model is to use :value and @input (:value="pageTitle" @input="(e) => pageTitle = e.target.value").
+        -->
+        <input id="" type="text" class="form-control" v-model="pageTitle">
+      </div>
+      <div class="mb-3">
+        <label for="" class="form-label">
+          Content
+        </label>
+        <textarea type="text" class="form-control" rows="5" v-model="content"></textarea>
+      </div>
+      <div class="mb-3">
+        <button class="btn btn-primary" @click.prevent="pageCreated({ pageTitle, content })">Create Page</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['pageCreated'],
+  data() {
+    return {
+      pageTitle: '',
+      content: '',
+    }
+  }
+}
+</script>
