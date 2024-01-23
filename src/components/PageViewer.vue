2cc6e6d373656f4716e57a260576b224a2e7cebf
phillipeismark <phillip_eismark@hotmail.dk>
2024-01-23 14:59:37 +0100

    refactor watcher, to not use options API, but globally available  method. using it inside og created, so it creates the watcher at that time.

--- src/components/PageViewer.vue
+++ src/components/PageViewer.vue
@@ -8,16 +8,19 @@
 <script>
 
 export default {
-  watch: { // watch the url param, and when it changes, update the component.
-    '$route.params.index': { // This is the index passed via the URL made accessible on the $route object due to vue-router plugin added in main.js
-      fetchPage(newIndex) { // Method to run when the dependency ($route.params.index) changes.
-        this.page = this.$pages.getSinglePage(newIndex); // Use the data store to fetch the page.
-      },
-    },
-  },
+  // watch: { // watch the url param, and when it changes, update the component.
+  //   '$route.params.index': { // This is the index passed via the URL made accessible on the $route object due to vue-router plugin added in main.js
+  //     fetchPage(newIndex) { // Method to run when the dependency ($route.params.index) changes.
+  //       this.page = this.$pages.getSinglePage(newIndex); // Use the data store to fetch the page.
+  //     },
+  //   },
+  // },
   created() { // lifecycle fn is not updating the component, because it is cached. So when we navigate from one page to another, that has the same component, it displays the same data, so we have to use watch.
     // Combine the url param with the storage object, to fetch the page the REST way.
     this.page = this.$pages.getSinglePage(this.$route.params.index);
+    this.$watch(() => this.$route.params.index, (newValue, prevValue) => { // 1st argument is the fn that returns what we watch, 2nd is the fn that executes when the 1st fn returns a different value. 
+      this.page = this.$pages.getSinglePage(newValue);
+    })
   },
   data() {
     return {
