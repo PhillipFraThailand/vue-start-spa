4f3cd7717ff63f6d3e12b9401fdda7b10a86661c
phillipeismark <phillip_eismark@hotmail.dk>
2024-01-23 15:08:49 +0100

    implement a 3rd way of watching for changes on the PageViewer component, by setting props up via routes. so url params are passed as props

--- src/components/PageViewer.vue
+++ src/components/PageViewer.vue
@@ -7,7 +7,13 @@
 
 <script>
 
+// Will display 3 ways of watching for changes, that account for component caching.
+// 1st is watching the global $route.params.index.
+// 2nd is using created lifecycle hook and adding a watcher in there.
+// 3rd is passing the url param as a prop to the component from routes.js, and watching that prop.
 export default {
+  props: ['index'],
+  // 1st way
   // watch: { // watch the url param, and when it changes, update the component.
   //   '$route.params.index': { // This is the index passed via the URL made accessible on the $route object due to vue-router plugin added in main.js
   //     fetchPage(newIndex) { // Method to run when the dependency ($route.params.index) changes.
@@ -16,11 +22,19 @@ export default {
   //   },
   // },
   created() { // lifecycle fn is not updating the component, because it is cached. So when we navigate from one page to another, that has the same component, it displays the same data, so we have to use watch.
-    // Combine the url param with the storage object, to fetch the page the REST way.
-    this.page = this.$pages.getSinglePage(this.$route.params.index);
-    this.$watch(() => this.$route.params.index, (newValue, prevValue) => { // 1st argument is the fn that returns what we watch, 2nd is the fn that executes when the 1st fn returns a different value. 
+    this.page = this.$pages.getSinglePage(this.index);
+
+    // 2nd way
+    // this.$watch(() => this.$route.params.index, (newValue, prevValue) => { // 1st argument is the fn that returns what we watch, 2nd is the fn that executes when the 1st fn returns a different value. 
+    //   this.page = this.$pages.getSinglePage(newValue);
+    // })
+  },
+
+  // 3rd way. This way does not watch the global $route.params, but does require additional setup in routes.
+  watch: {
+    index(newValue, oldValue) {
       this.page = this.$pages.getSinglePage(newValue);
-    })
+    }
   },
   data() {
     return {
--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,9 @@ import CreatePage from './components/CreatePage.vue';
 const router = createRouter({
   history: createWebHashHistory(), // How the history is going to be treated in this router.
   routes: [ // Define the route matching to components.
-    { path: '/:index?', component: PageViewer }, // :index is a dynamic url parameter that the router will look for when accessing '/'. Adding ? makes the url param optional.
+    // :index is an url parameter. The router will look for it when accessing '/' ? in front makes the url param optional.
+    // Adding props:true will make the router pass the url parameters as props. This way 
+    { path: '/:index?', component: PageViewer, props: true }, 
     { path: '/create', component: CreatePage },
   ]
 })
